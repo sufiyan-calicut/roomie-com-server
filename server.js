888a1023d8567c8f9657d5591c3fff21ecb128ef
sufiyan-calicut <115002412+sufiyan-calicut@users.noreply.github.com>
2023-10-24 07:09:02 +0530

    bugs fixed

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 app.use((err, req, res, next) => {
-  console.error(err);
+  console.error('Error:', err);
   res.status(500).json({ message: 'Internal server error' });
 });
 
